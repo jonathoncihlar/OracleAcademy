de778d8ac21834a16b4afc48456c923b5bde71a2
jonathoncihlar <jonathoncihlar@github.com>
2023-09-29 16:34:14 -0600

    Fix white space on import in Section 6 Problem 2.

--- Section 6/DisplayMultiples.java
+++ Section 6/DisplayMultiples.java
@@ -7,8 +7,7 @@
  * @version 1.0
  */
 
-
- import java.util.Scanner;
+import java.util.Scanner;
 public class DisplayMultiples {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
